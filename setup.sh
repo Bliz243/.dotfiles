c33f5f20f4cdf82a1d531def761e7b008324a5cb
Bliz243 <jacobjacob243@hotmail.com>
2023-03-26 05:29:54 +0200

    Changed setup

--- setup.sh
+++ setup.sh
@@ -11,12 +11,6 @@ echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/microsoft.gpg] https://pa
 sudo apt update
 sudo apt install -y code
 
-# Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install zsh-pluggins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
 # Install font-logos
 wget -O font-logos.zip https://github.com/lukas-w/font-logos/archive/refs/heads/master.zip
 unzip font-logos.zip
@@ -30,13 +24,21 @@ sudo cp JetBrainsMono-font/*.ttf /usr/local/share/fonts
 sudo fc-cache -f -v
 
 # Install starship
-curl -fsSL https://starship.rs/install.sh | bash
+curl -fsSL https://starship.rs/install.sh | sh
 
 # Install exa
-wget -O exa-linux-x86_64-v0.10.1.zip https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
-unzip exa-linux-x86_64-v0.10.1.zip
-sudo mv exa-linux-x86_64 /usr/local/bin/exa
-rm exa-linux-x86_64-v0.10.1.zip
+sudo apt update
+sudo apt install -y unzip
+EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
+sudo unzip -q exa.zip bin/exa -d /usr/local
+rm -rf exa.zip
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install zsh-pluggins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Symlink .zshrc and starship.toml to the home directory
 ln -sf "$(pwd)/zsh/.zshrc" "$HOME/.zshrc"
